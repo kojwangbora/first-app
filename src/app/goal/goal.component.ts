04866fa3521e205333759b55b6906aa114bfe488
kojwangbora <kojwangbora254@gmail.com>
2022-02-05 00:35:29 +0300

    comms

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
 
 @Component({
   selector: 'app-goal',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalComponent implements OnInit {
 
+  goals:Goal[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+    
+  ];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
