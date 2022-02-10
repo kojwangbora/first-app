968df52924eda6493fd624fdc59a6bd3d408c272
kojwangbora <kojwangbora254@gmail.com>
2022-02-10 12:48:27 +0300

    bootstrap

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -8,10 +8,11 @@ import { Goal } from '../goal';
 })
 export class GoalDetailComponent implements OnInit {
 
-  @Input() goal!: Goal;
+  @Input()
+  goal!: Goal;
   @Output() isComplete = new EventEmitter<boolean>();
 
-  goalComplete(complete:boolean){
+  goalDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
   constructor() { }
